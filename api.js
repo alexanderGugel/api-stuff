f023ec7b440a04349843b32020db53ce9573b8a4
Alexander Gugel <alexander.gugel@gmail.com>
2014-10-30 14:06:59 +0100

    Fix CORS

--- api.js
+++ api.js
@@ -5,6 +5,12 @@ var api = express.Router();
 
 var expire = process.env.CACHE_EXPIRE || 3600;
 
+api.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 api.get('*', function (req, res, next) {
   redis.get(req.url, function (error, cached) {
     if (cached) {
@@ -22,12 +28,6 @@ api.use(function (req, res, next) {
     redis.expire(req.url, expire);
   };
   next()
-});
-
-api.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
 });
 
 api.use(require('./vine'));
